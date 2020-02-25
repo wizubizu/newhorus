a4f50b0b6570e7c86c6f5778c35005fbd8379833
wizubizu <odugbesanabimbola@gmail.com>
2020-02-25 18:23:57 +0100

    horus

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+// dashboard show and hide
+    $('#openSidebar').click(function(){
+    $('.leftbar').addClass('biger');
+    });
+    $('#closebtn').click(function(){
+        $('.leftbar').removeClass('biger');
+    });
